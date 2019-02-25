d24f28a607532ec6c2e2fdd35152d8ef8dc133f8
JakubWagner1019 <jakub.wagner.1019@gmail.com>
2019-02-25 12:05:09 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -7,32 +7,44 @@ def sigmoid(x):
 def sigmoid_derivative(x):
 	return sigmoid(x)*(1-sigmoid(x))
 	
-class NeuralNet:
-	def __init__(self,input_size,hidden_size,output_size):
-		self.input_size = input_size
-		self.hidden_size = hidden_size
-		self.output_size = output_size
-		self.W1 = np.random.uniform(size = (self.input_size,self.hidden_size))
-		self.W2 = np.random.uniform(size = (self.hidden_size,self.output_size))
-	def learn(self,input,output,learning_rate):
-		A1 = sigmoid(np.dot(input,self.W1))
-		yhat = sigmoid(np.dot(A1,self.W2))
-		E = yhat - output
-		delta1 = E * learning_rate * sigmoid_derivative(yhat)
-		self.W2 -= A1.T.dot(delta1)
-		delta2 = delta1.dot(self.W2.T) * sigmoid_derivative(A1)
-		self.W1 -= input.T.dot(delta2)
-	def run(self,input):
-		A1 = sigmoid(np.dot(input,self.W1))
-		return sigmoid(np.dot(A1,self.W2))
+def scale(x,in_min,in_max,out_min,out_max):
+	return (out_max-out_min)/(in_max-in_min)*(x-in_min)+out_min
 	
+def normalize(x,in_min,in_max):
+	return scale(x,in_min,in_max,-1,1)
+
+class NeuralNet:
+	def __init__(self,input_size,output_size,hidden_size):
+		self.input_size = input_size
+		self.output_size = output_size
+		in_sizes = [input_size,*hidden_size]
+		out_sizes = [*hidden_size,output_size]
+		self.W = list(np.random.uniform(size = a) for a in zip(in_sizes,out_sizes))
+	def learn(self,input,output,learning_rate):
+		out = self.run(input)
+		E = out - output
+		delta = E * learning_rate * sigmoid_derivative(out)
+		for i in reversed(range(len(self.W))):
+			self.W[i] -= self.A[i].T.dot(delta)
+			delta = delta.dot(self.W[i].T) * sigmoid_derivative(self.A[i])
+	def run(self,input):
+		self.A = list([] for _ in range(len(self.W)+1))
+		self.A[0] = input
+		for i,W in enumerate(self.W):
+			self.A[i+1] = sigmoid(np.dot(self.A[i],W))
+		return self.A[-1]
+
 if __name__=="__main__":
-	a = NeuralNet(2,3,1)
 	X = np.array([[0,0],[0,1],[1,0],[1,1]])
-	Y = np.array([[1],[0],[1],[0]])
+	Y = np.array([[0],[1],[1],[0]])
+	input_size = 2
+	output_size = 1
+	hidden_size = [3]
+	print("input size: {} output size: {} hidden size: {}".format(input_size,output_size,hidden_size))
+	a = NeuralNet(input_size,output_size,hidden_size)
 	for i in range(50000):
 		a.learn(X,Y,0.1)
-	print("input: {} output: {}".format([0,0],*(a.run([0,0]))))
-	print("input: {} output: {}".format([0,1],*(a.run([0,1]))))
-	print("input: {} output: {}".format([1,0],*(a.run([1,0]))))
-	print("input: {} output: {}".format([1,1],*(a.run([1,1]))))
+	print("input: {} output: {}".format([0,0],(a.run([0,0]))))
+	print("input: {} output: {}".format([0,1],(a.run([0,1]))))
+	print("input: {} output: {}".format([1,0],(a.run([1,0]))))
+	print("input: {} output: {}".format([1,1],(a.run([1,1]))))
